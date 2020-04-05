859298779d42aea37b71a437f51c8542a21f1691
lmelendez17 <lmelendez17@kentdenver.org>
2020-04-05 11:38:24 -0600

    initial commit to branch

---
+++ cse237S-FinalProject/src/tests/AStarAlgorithmTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import code.AStar.AStar;
+
+class AStarAlgorithmTest {
+
+	@Test
+	void test() {
+		boolean[][] walls = AStar.createWalls();
+		int startx = 3, starty = 3;
+		AStar as = new AStar(walls, startx, starty, 8, 8);
+		AStar.printPath(walls, as.getPath(), startx, starty);
+	}
+
+}
