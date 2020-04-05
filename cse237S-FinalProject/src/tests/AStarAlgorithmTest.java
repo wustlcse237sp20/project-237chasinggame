62186cd20169a53584a71572e6bc19b5bca9130c
lmelendez17 <lmelendez17@kentdenver.org>
2020-04-05 12:31:24 -0600

    tests finished

--- cse237S-FinalProject/src/tests/AStarAlgorithmTest.java
+++ cse237S-FinalProject/src/tests/AStarAlgorithmTest.java
@@ -1,6 +1,10 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +13,79 @@ import code.AStar.AStar;
 class AStarAlgorithmTest {
 
 	@Test
-	void test() {
+	void positionStartSetTest() {
 		boolean[][] walls = AStar.createWalls();
-		int startx = 3, starty = 3;
-		AStar as = new AStar(walls, startx, starty, 8, 8);
-		AStar.printPath(walls, as.getPath(), startx, starty);
+		int width = walls.length, height = walls[0].length;
 		
+		for(int i=1; i<width-1; i++) {
+			for(int j=1; j<height-1; j++) {
+				int startx = i, starty = j;
+				AStar as = new AStar(walls, startx, starty, 8, 8);
+				assertEquals(as.getStartX(), startx);
+				assertEquals(as.getStartY(), starty);
+			}	
+		}
+	}
+
+	@Test
+	void positionEndSetTest() {
+		boolean[][] walls = AStar.createWalls();
+		int width = walls.length, height = walls[0].length;
+		
+		for(int i =1; i<width-1; i++) {
+			for(int j =1; j<height-1; j++) {
+				int endx = i, endy = j;
+				AStar as = new AStar(walls, 1, 1, endx, endy);
+				assertEquals(as.getEndX(), endx);
+				assertEquals(as.getEndY(), endy);
+			}	
+		}
+	}
+	
+	@Test
+	void findsEndTest() {
+		boolean[][] walls = AStar.createWalls();
+		int width = walls.length, height = walls[0].length;
+		int startx = 4, starty = 4;
+		
+		for(int i=1; i<width-1; i++) {
+			for(int j =1; j<height-1; j++) {
+				if((i>=3 || i<=5) && (j>=3 || j<=5)) {
+					continue;
+				}
+				int endx = i, endy = j;
+				AStar as = new AStar(walls, startx, starty, endx, endy);
+				ArrayList<int[]> visitedPoints = AStar.printPath(walls, as.getPath(), startx, starty);
+				int [] lastPoint = visitedPoints.get(visitedPoints.size()-1);
+				assertEquals(lastPoint[0], endx);
+				assertEquals(lastPoint[1], endy);
+			}
+			
+		}
+
+	}
+	
+	@Test
+	void numberOfMoves() {
+		boolean[][] walls = AStar.createWalls();
+		int width = walls.length, height = walls[0].length;
+		int startx = 4, starty = 4;
+		
+		for(int i=1; i<width-1; i++) {
+			for(int j =1; j<height-1; j++) {
+				if((i>=3 || i<=5) && (j>=3 || j<=5)) {
+					continue;
+				}
+				int endx = i, endy = j;
+				int minMoves = (int)Math.sqrt(Math.pow(i,2)+Math.pow(j,2));
+				AStar as = new AStar(walls, startx, starty, endx, endy);
+				int numMoves = as.getPath().size();
+				assertTrue(numMoves>=minMoves);
+			
+			}
+			
+		}
+
 	}
 
 }
